842a69297bfe4ef46d0c77a446dbbaaed3183b92
Kyle <kyletlee004@gmail.com>
2025-04-14 22:01:02 -0400

    Attempting to fix cors

--- src/main/java/com/example/utils/CorsHeaders.java
+++ src/main/java/com/example/utils/CorsHeaders.java
@@ -7,8 +7,9 @@ public class CorsHeaders {
     public static Map<String, String> getCorsHeaders() {
         Map<String, String> headers = new HashMap<>();
         headers.put("Access-Control-Allow-Origin", "*");
+        headers.put("Access-Control-Allow-Credentials", "true");
+        headers.put("Access-Control-Allow-Methods", "OPTIONS,POST,GET,PUT,DELETE");
         headers.put("Access-Control-Allow-Headers", "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token");
-        headers.put("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
         return headers;
     }
 } 
